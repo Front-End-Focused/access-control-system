922346cce4ea1472fbdb3fddfc093abfefeb962a
elmahdim <elmahdi.mahmoud@live.com>
2021-09-13 17:28:22 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import doorOpen from "./assets/door-open.png";
+import doorclosed from "./assets/door-closed.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    door: false,
+  };
+
+  handleVisitor(id) {
+    if (this.state.door) return;
+
+    if (this.props.whilelist.includes(id)) {
+      this.openDoor();
+      setTimeout(() => {
+        this.closeDoor();
+      }, 1500);
+    } else {
+      this.closeDoor();
+    }
+  }
+
+  openDoor() {
+    this.setState({ door: true });
+  }
+
+  closeDoor() {
+    this.setState({ door: false });
+  }
+
+  render() {
+    const { visitors } = this.props;
+    const { door } = this.state;
+
+    return (
+      <div className="control">
+        <div className="door-frame">
+          <img src={door ? doorOpen : doorclosed} alt="Door" />
+        </div>
+        <div className="visitors row">
+          {visitors.map((visitor) => (
+            <img
+              src={visitor.icon}
+              alt={visitor.id}
+              key={visitor.id}
+              role="button"
+              onClick={() => this.handleVisitor(visitor.id)}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import person1 from "./assets/person-1.svg";
+import person2 from "./assets/person-2.svg";
+import person3 from "./assets/person-3.svg";
+import person4 from "./assets/person-4.svg";
+import person5 from "./assets/person-5.svg";
+import person6 from "./assets/person-6.svg";
+
+const visitors = [
+  {
+    id: 1,
+    icon: person1,
+  },
+  {
+    id: 2,
+    icon: person2,
+  },
+  {
+    id: 3,
+    icon: person3,
+  },
+  {
+    id: 4,
+    icon: person4,
+  },
+  {
+    id: 5,
+    icon: person5,
+  },
+  {
+    id: 6,
+    icon: person6,
+  },
+];
+
+const whilelist = [2, 3, 4, 5];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App visitors={visitors} whilelist={whilelist} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
